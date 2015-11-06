c6b66fd6d87b0d422aa25bb56bef11a6c44bd679
korytkoBT <korytko07@gmail.com>
2015-11-06 23:48:23 +0100

    Working WIth SetNatural

---
+++ BT-Library-Java/src/Bt/Tests/Core/Collections/AllTestsCollections.java
@@ -0,0 +1,13 @@
+package Bt.Tests.Core.Collections;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+	TestQueueNatural.class,
+})
+
+public class AllTestsCollections{
+
+}
