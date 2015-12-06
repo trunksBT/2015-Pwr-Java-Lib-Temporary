897f57f02272cff4cb47339d16d3c7dbf5cbd7be
korytkoBT <korytko07@gmail.com>
2015-12-06 04:20:40 +0000

    Working With Shapes Part1

---
+++ BT-Library-Java/src/Bt/Core/ABC/Tests/Circle.java
@@ -0,0 +1,34 @@
+package Bt.Core.ABC.Tests;
+
+import Bt.Core.Collections.Shape;
+import Bt.Core.Geometry.Complex.LineSegment;
+import Bt.Core.Geometry.Elementaries.Point;
+
+public class Circle extends Shape {
+
+	Point center;
+	LineSegment radius;
+	
+	public Circle(String name)
+	{
+		super(name);
+	}
+	
+	public Circle(String name, Point center,LineSegment radius)
+	{
+		super(name);
+		this.center = center;
+		this.radius = radius;
+	}
+
+	@Override
+	public double Obwod() {
+		return 2*Math.PI*radius.length();
+	}
+
+	@Override
+	public String toString() {
+		return "[Circle_Name: "+name.toString()+center.toString()+"_Radius: "+radius.toString()+"]";
+	}
+	
+}
---
+++ BT-Library-Java/src/Bt/Core/Collections/Shape.java
@@ -0,0 +1,39 @@
+package Bt.Core.Collections;
+
+public abstract class Shape {
+	
+	public String name;
+	
+	public abstract double Obwod();
+
+	public abstract String toString();
+	
+	public Shape(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Shape other = (Shape) obj;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+}
