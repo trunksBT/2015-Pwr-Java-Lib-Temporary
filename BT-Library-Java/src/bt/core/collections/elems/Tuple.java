144a3e22002463a22ba607605a6c196a67888849
korytkoBT <209826@student.pwr.wroc.pl>
2016-02-24 16:29:26 +0000

    T1: System.arraycopy tested; T2: assertSame tested; T3: rewritten mutual recursion with printing folder tree

--- BT-Library-Java/src/bt/core/collections/elems/Tuple.java
+++ BT-Library-Java/src/bt/core/collections/elems/Tuple.java
@@ -11,6 +11,28 @@ public class Tuple<W,S> implements Comparable<Tuple<W,S>>{
 	}
 
 	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Tuple<W, S> other = (Tuple<W, S>) obj;
+		if (key == null) {
+			if (other.key != null)
+				return false;
+		} else if (!key.equals(other.key))
+			return false;
+		if (value == null) {
+			if (other.value != null)
+				return false;
+		} else if (!value.equals(other.value))
+			return false;
+		return true;
+	}
+
+	@Override
 	public int compareTo(Tuple<W,S> o) {
 		int retVal = 0;
 		
