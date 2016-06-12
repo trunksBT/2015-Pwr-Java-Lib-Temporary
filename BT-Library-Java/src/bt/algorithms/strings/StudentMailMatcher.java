0eab3614de8454eaf42afb54148a39a0c24fc31b
korytkoBT <209826@student.pwr.wroc.pl>
2016-06-12 16:48:01 +0000

    T1: Turnstile works to 12 test, nazwaKontaDlaStudenta tested; T2: Inserting further tests

--- BT-Library-Java/src/bt/algorithms/strings/StudentMailMatcher.java
+++ BT-Library-Java/src/bt/algorithms/strings/StudentMailMatcher.java
@@ -1,26 +1,60 @@
 package bt.algorithms.strings;
 
 public class StudentMailMatcher implements MailMatcher{
+	public final int STUDENT_IDX_SIZE = 6;
+	public final int SERWER_ADDRES_1_SIZE = 7;
+	public final int SERWER_ADDRES_2_SIZE = 3;
+	public final int SERWER_ADDRES_3_SIZE = 3;
+	public final int SERWER_ADDRES_4_SIZE = 2;
 	public static final StudentMailMatcher INSTANCE = new StudentMailMatcher();
 	
 	private StudentMailMatcher() {}
 
 	@Override
 	public boolean apply(String inChain) {
-		StringBuilder uppCased = new StringBuilder(inChain);
-//		uppCased
+		String upCased = StringUtils.upCase(inChain);
 		boolean result = true;
 		char curr;
+		int studentIdxSize = 0;
+		int serwerAddres1Size = 0;
 		int it = -1;
 		
-		if((curr = inChain.charAt(++it)) == '@')
+		// isAccountNameNotEmpty
+		if((curr = upCased.charAt(++it)) == '@')
 				result&= false;
-		
-		while( (curr = inChain.charAt(it++)) != '@')
+
+		// isAccountNameConsistedFromChars
+		while( (curr = upCased.charAt(it++)) != '@' && result)
 		{
-			result&= !Character.isLetter(curr);
+			if(!Character.isLetter(curr)) {
+				result&= true;
+				studentIdxSize++;
+			}else
+				result&= false;
 		}
 
+		// isAccountNameConsistedFrom6Numbers
+		if( studentIdxSize != STUDENT_IDX_SIZE)
+			result = false;	
+		
+		// isSerwerAdressNotEmpty
+		if((curr = upCased.charAt(it)) == '.' && result)
+				result&= false;
+		
+		// isSerwerAdressConsistedFromChars
+		while( (curr = upCased.charAt(it++)) != '.' && result)
+		{
+			if(Character.isLetter(curr)) {
+				result&= true;
+				serwerAddres1Size++;
+			}else
+				result&= false;
+		}
+		
+		// isSerwerAdressConsistedFrom3Letters
+		if( serwerAddres1Size != SERWER_ADDRES_1_SIZE && result)
+			result = false;
+		
 		
 		return result;
 	}
--- BT-Library-Java/test/bt/algorithms/strings/TestStudentMailMatcher.java
+++ BT-Library-Java/test/bt/algorithms/strings/TestStudentMailMatcher.java
@@ -20,21 +20,203 @@ public class TestStudentMailMatcher extends TestCase
 	@Before
 	public void setUp() throws Exception {
 		tcMock = new ArrayList<>(ammOfTc);
-		tcMock.add("@student.pwr.edu.pl");
-		tcMock.add("a@student.pwr.edu.pl");
+		/* 0  */ tcMock.add("@student.pwr.edu.pl");
+		/* 1  */ tcMock.add("a@student.pwr.edu.pl");
+		/* 2  */ tcMock.add("2@student.pwr.edu.pl");
+		/* 3  */ tcMock.add("23@student.pwr.edu.pl");
+		/* 4  */ tcMock.add("000000@student.pwr.edu.pl");
+		/* 5  */ tcMock.add("0000000@student.pwr.edu.pl");
+		/* 6  */ tcMock.add("000001@student.pwr.edu.pl");
+		/* 7  */ tcMock.add("00000a@student.pwr.edu.pl");
+		/* 8  */ tcMock.add("asdzxc@student.pwr.edu.pl");
+		/* 9  */ tcMock.add("000001@@student.pwr.edu.pl");
+		/* 10 */ tcMock.add("000001 @student.pwr.edu.pl");
+		/* 11 */ tcMock.add("000001student.pwr.edu.pl");
+		/* 12 */ tcMock.add("000000@.pwr.edu.pl");
 	}
+
 
 	@After
 	public void tearDown() throws Exception {
 		System.out.println( "TearDown" );
 	}
-
+	
 	//Test_MethodName_StateUnderTest_Expect_ExpectedBehavior
 	@Test
-	public void test_0_nazwa_Konta_0_Znakow_Expect_false()
+	public void test_12_wczesniejOk_adresSerwera1_Brak_Expect_false()
 	{
 	    //arrange
-		int tcIdx = 0;
+		int tcIdx = 12;
+		boolean rcVal = true;
+		String inChain = tcMock.get(tcIdx);
+		MailMatcher turnstile = StudentMailMatcher.INSTANCE;
+		
+		//act
+		rcVal = turnstile.apply(inChain);
+		
+		//assert
+		assertFalse(rcVal);
+	}
+	
+	//Test_MethodName_StateUnderTest_Expect_ExpectedBehavior
+	@Test
+	public void test_11_nazwa_Konta_6_Cyfr_BrakMalpy_Expect_false()
+	{
+	    //arrange
+		int tcIdx = 11;
+		boolean rcVal = true;
+		String inChain = tcMock.get(tcIdx);
+		MailMatcher turnstile = StudentMailMatcher.INSTANCE;
+		
+		//act
+		rcVal = turnstile.apply(inChain);
+		
+		//assert
+		assertFalse(rcVal);
+	}
+	
+	//Test_MethodName_StateUnderTest_Expect_ExpectedBehavior
+	@Test
+	public void test_10_nazwa_Konta_6_Cyfr_1_Spacja_Expect_false()
+	{
+	    //arrange
+		int tcIdx = 10;
+		boolean rcVal = true;
+		String inChain = tcMock.get(tcIdx);
+		MailMatcher turnstile = StudentMailMatcher.INSTANCE;
+		
+		//act
+		rcVal = turnstile.apply(inChain);
+		
+		//assert
+		assertFalse(rcVal);
+	}
+	
+	//Test_MethodName_StateUnderTest_Expect_ExpectedBehavior
+	@Test
+	public void test_9_nazwa_Konta_6_Cyfr_2xMalpa_Expect_false()
+	{
+	    //arrange
+		int tcIdx = 9;
+		boolean rcVal = true;
+		String inChain = tcMock.get(tcIdx);
+		MailMatcher turnstile = StudentMailMatcher.INSTANCE;
+		
+		//act
+		rcVal = turnstile.apply(inChain);
+		
+		//assert
+		assertFalse(rcVal);
+	}
+	
+	//Test_MethodName_StateUnderTest_Expect_ExpectedBehavior
+	@Test
+	public void test_8_nazwa_Konta_6_Znakow_Expect_false()
+	{
+	    //arrange
+		int tcIdx = 8;
+		boolean rcVal = true;
+		String inChain = tcMock.get(tcIdx);
+		MailMatcher turnstile = StudentMailMatcher.INSTANCE;
+		
+		//act
+		rcVal = turnstile.apply(inChain);
+		
+		//assert
+		assertFalse(rcVal);
+	}
+	
+	//Test_MethodName_StateUnderTest_Expect_ExpectedBehavior
+	@Test
+	public void test_7_nazwa_Konta_5_Cyfry_1_Znak_Expect_false()
+	{
+	    //arrange
+		int tcIdx = 7;
+		boolean rcVal = true;
+		String inChain = tcMock.get(tcIdx);
+		MailMatcher turnstile = StudentMailMatcher.INSTANCE;
+		
+		//act
+		rcVal = turnstile.apply(inChain);
+		
+		//assert
+		assertFalse(rcVal);
+	}
+	
+	//Test_MethodName_StateUnderTest_Expect_ExpectedBehavior
+	@Test
+	public void test_6_nazwa_Konta_6_Cyfra_Expect_true()
+	{
+	    //arrange
+		int tcIdx = 6;
+		boolean rcVal = true;
+		String inChain = tcMock.get(tcIdx);
+		MailMatcher turnstile = StudentMailMatcher.INSTANCE;
+		
+		//act
+		rcVal = turnstile.apply(inChain);
+		
+		//assert
+		assertTrue(rcVal);
+	}
+	
+	//Test_MethodName_StateUnderTest_Expect_ExpectedBehavior
+	@Test
+	public void test_5_nazwa_Konta_7_Cyfra_Expect_false()
+	{
+	    //arrange
+		int tcIdx = 5;
+		boolean rcVal = true;
+		String inChain = tcMock.get(tcIdx);
+		MailMatcher turnstile = StudentMailMatcher.INSTANCE;
+		
+		//act
+		rcVal = turnstile.apply(inChain);
+		
+		//assert
+		assertFalse(rcVal);
+	}
+	
+	//Test_MethodName_StateUnderTest_Expect_ExpectedBehavior
+	@Test
+	public void test_4_nazwa_Konta_6_Cyfra_Expect_true()
+	{
+	    //arrange
+		int tcIdx = 4;
+		boolean rcVal = true;
+		String inChain = tcMock.get(tcIdx);
+		MailMatcher turnstile = StudentMailMatcher.INSTANCE;
+		
+		//act
+		rcVal = turnstile.apply(inChain);
+		
+		//assert
+		assertTrue(rcVal);
+	}
+	
+	//Test_MethodName_StateUnderTest_Expect_ExpectedBehavior
+	@Test
+	public void test_3_nazwa_Konta_2_Cyfra_Expect_false()
+	{
+	    //arrange
+		int tcIdx = 3;
+		boolean rcVal = true;
+		String inChain = tcMock.get(tcIdx);
+		MailMatcher turnstile = StudentMailMatcher.INSTANCE;
+		
+		//act
+		rcVal = turnstile.apply(inChain);
+		
+		//assert
+		assertFalse(rcVal);
+	}
+	
+	//Test_MethodName_StateUnderTest_Expect_ExpectedBehavior
+	@Test
+	public void test_2_nazwa_Konta_1_Cyfra_Expect_false()
+	{
+	    //arrange
+		int tcIdx = 2;
 		boolean rcVal = true;
 		String inChain = tcMock.get(tcIdx);
 		MailMatcher turnstile = StudentMailMatcher.INSTANCE;
@@ -52,6 +234,23 @@ public class TestStudentMailMatcher extends TestCase
 	{
 	    //arrange
 		int tcIdx = 1;
+		boolean rcVal = true;
+		String inChain = tcMock.get(tcIdx);
+		MailMatcher turnstile = StudentMailMatcher.INSTANCE;
+		
+		//act
+		rcVal = turnstile.apply(inChain);
+		
+		//assert
+		assertFalse(rcVal);
+	}
+	
+	//Test_MethodName_StateUnderTest_Expect_ExpectedBehavior
+	@Test
+	public void test_0_nazwa_Konta_0_Znakow_Expect_false()
+	{
+	    //arrange
+		int tcIdx = 0;
 		boolean rcVal = true;
 		String inChain = tcMock.get(tcIdx);
 		MailMatcher turnstile = StudentMailMatcher.INSTANCE;
