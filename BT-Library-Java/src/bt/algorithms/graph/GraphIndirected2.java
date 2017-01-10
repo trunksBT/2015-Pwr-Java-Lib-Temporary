561ca2ad1d17f8d865db5b6c53d1dfeb17afb679
korytkoBT <209826@student.pwr.wroc.pl>
2017-01-10 18:13:41 +0000

    Add krawedzie i grafNieskierowany

--- BT-Library-Java/src/bt/algorithms/graph/GraphIndirected2.java
+++ BT-Library-Java/src/bt/algorithms/graph/GraphIndirected2.java
@@ -8,70 +8,70 @@ import java.util.List;
 
 import bt.algorithms.interfaces.IGraf;
 
-public class GraphIndirected2<W,S> implements IGraf<W,S>{
+public class GraphIndirected2<W,S> implements IGraf<W,S>
+{
 	private List<W> wierzcholki_;
-	private S[][] polaczenia_;
-	private Hashtable<W,Integer> indexWierzcholka;
+	private S[][] krawedzie_;
+	private Hashtable<W, Integer> indexyWierzcholkow;
 	
-	public GraphIndirected2(List<W> wierzcholki, S[][] polaczenia)
+	public GraphIndirected2(List<W> wierzcholki, S[][] krawedzie)
 	{
-		wierzcholki_ = wierzcholki == null ? new ArrayList<W>() : wierzcholki;
-		polaczenia_ = polaczenia;
+		wierzcholki_ = wierzcholki == null ? new ArrayList<W>():wierzcholki;
+		krawedzie_ = krawedzie;
 		
-		if(wierzcholki_ != null && wierzcholki_.size() != 0)
-		{		
-			indexWierzcholka = new Hashtable<>(wierzcholki_.size());
-			int idx = 0;
-			Iterator it = wierzcholki_.iterator();
-			while(it.hasNext())
+		indexyWierzcholkow = ustawIndexyWierzcholkow();
+	}
+	
+	Hashtable<W, Integer> ustawIndexyWierzcholkow()
+	{
+		Hashtable<W, Integer> mapaIndexow = new Hashtable<W, Integer>();
+		int idx = 0;
+		Iterator it = wierzcholki_.iterator();
+		
+		while(it.hasNext())
+		{
+			W curr = (W) it.next();
+			if(curr == null)
 			{
-				W curr = (W) it.next();
-				if(curr == null)
-				{
-					throw new NullPointerException();
-				}
-				else
-				{
-					indexWierzcholka.put(curr, idx++);				
-				}
+				throw new NullPointerException();
+			}
+			else
+			{
+				mapaIndexow.put(curr, idx++);
 			}
 		}
+		
+		return mapaIndexow;
 	}
-
+	
 	@Override
 	public List<W> Wierzcholki() {
-		return wierzcholki_; 
+		return wierzcholki_;
 	}
 
 	@Override
 	public S Krawedz(W w1, W w2) {
-		if(polaczenia_ != null && w1 != null && w2 != null
-				&& wierzcholki_.contains(w1) && wierzcholki_.contains(w2))
+		if(krawedzie_ != null && w1 != null && w2 != null && wierzcholki_.contains(w1) && wierzcholki_.contains(w2))
 		{
-			return polaczenia_[indexWierzcholka.get(w1)][indexWierzcholka.get(w2)];
+			return krawedzie_[indexyWierzcholkow.get(w1)][indexyWierzcholkow.get(w2)];
 		}
 		return null;
 	}
 
 	@Override
 	public List<W> Krawedzie(W w) {
-		List<W> krawedzie = new LinkedList<W>();
-		
-		if(w != null && wierzcholki_.contains(w) && indexWierzcholka != null && polaczenia_!=null)
+		List<W> krawedzie = new LinkedList<>();
+		if(w != null && krawedzie_ != null && indexyWierzcholkow != null && wierzcholki_.contains(w))
 		{
-			S[] polaczenia = polaczenia_[indexWierzcholka.get(w)];
-			if(polaczenia != null)
+			S[] polaczoneWierzcholki = krawedzie_[indexyWierzcholkow.get(w)];
+			for(int i = 0; i < wierzcholki_.size(); i++)
 			{
-				for(int i = 0; i < wierzcholki_.size(); i++)
+				if(polaczoneWierzcholki[i] != null)
 				{
-					if(polaczenia[i] != null)
-					{
-						krawedzie.add(wierzcholki_.get(i));
-					}
+					krawedzie.add(wierzcholki_.get(i));
 				}
 			}
 		}
-		
 		return krawedzie;
 	}
 }
---
+++ BT-Library-Java/src/bt/algorithms/interfaces/IKrawedz.java
@@ -0,0 +1,7 @@
+package bt.algorithms.interfaces;
+
+public interface IKrawedz<W, S> {
+	W w1();
+	W w2();
+	S wart();
+}
